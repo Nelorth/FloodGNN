2976f8e67eb14237df33825bf62600a0db932651
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-03-28 12:15:16 +0100

    update

--- lamah_dataset.py
+++ lamah_dataset.py
@@ -1,3 +1,4 @@
+import itertools
 import pandas as pd
 import tarfile
 import torch
@@ -7,18 +8,27 @@ from torch_geometric.data import Data, Dataset
 from tqdm import tqdm
 
 
+# manual impl as long as Colab hasn't updated to Python 3.10
+def pairwise(iterable):
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
 class LamaHDataset(Dataset):
     DATA_URL = "https://zenodo.org/record/5153305/files/1_LamaH-CE_daily_hourly.tar.gz"
 
-    def __init__(self, root, years=range(2000, 2018), window_size_hrs=24, window_stride_hrs=1, lead_time_hrs=1,
-                 normalize=True, transform=None, pre_transform=None, pre_filter=None):
-        super().__init__(root, transform, pre_transform, pre_filter)  # calls download() and process() if necessary
+    def __init__(self, root, years=range(2000, 2018), window_size_hrs=24, stride_length_hrs=1, lead_time_hrs=1,
+                 bidirectional=False, normalize=False):
+        super().__init__(root)  # calls download() and process() if necessary
 
         self.window_size_hrs = window_size_hrs
-        self.window_stride_hrs = window_stride_hrs
+        self.stride_length_hrs = stride_length_hrs
         self.lead_time_hrs = lead_time_hrs
+        self.years = years
         self.year_sizes = [
-            (24 * (365 + int(year % 4 == 0)) - (window_size_hrs + lead_time_hrs)) // window_stride_hrs + 1
+            (24 * (365 + int(year % 4 == 0)) - (window_size_hrs + lead_time_hrs)) // stride_length_hrs + 1
             for year in years]
 
         adjacency = pd.read_csv(self.processed_paths[0])
@@ -28,6 +38,14 @@ class LamaHDataset(Dataset):
         self.edge_index = torch.tensor(edge_cols.values.transpose(), dtype=torch.long)
         weight_cols = adjacency[["dist_hdn", "elev_diff", "strm_slope"]]
         self.edge_attr = torch.tensor(weight_cols.values, dtype=torch.float)
+        if bidirectional:
+            self.edge_index = torch.cat((self.edge_index, self.edge_index[(1, 0), :]), dim=1)
+            self.edge_attr = self.edge_attr.repeat(2, 1)
+
+        statistics = pd.read_csv(self.processed_paths[1], index_col="ID")
+        self.mean = torch.tensor(statistics["mean"].values, dtype=torch.float)
+        self.std = torch.tensor(statistics["std"].values, dtype=torch.float)
+
 
         self.year_tensors = [[] for _ in years]
         print("Loading dataset into memory...")
@@ -35,7 +53,7 @@ class LamaHDataset(Dataset):
             df = pd.read_csv(f"{self.raw_dir}/{self.raw_file_names[1]}/hourly/ID_{gauge_id}.csv",
                              sep=";", usecols=["YYYY", "qobs"])
             if normalize:
-                df["qobs"] = (df["qobs"] - df["qobs"].mean()) / df["qobs"].std()
+                df["qobs"] = (df["qobs"] - statistics.loc[gauge_id, "mean"]) / statistics.loc[gauge_id, "std"]
             for i, year in enumerate(years):
                 self.year_tensors[i].append(torch.tensor(df[df["YYYY"] == year]["qobs"].values, dtype=torch.float))
         self.year_tensors[:] = map(torch.stack, self.year_tensors)
@@ -46,7 +64,7 @@ class LamaHDataset(Dataset):
 
     @property
     def processed_file_names(self):
-        return ["adjacency.csv"]
+        return ["adjacency.csv", "statistics.csv"]
 
     def download(self):
         print("Downloading LamaH-CE from Zenodo to", self.raw_dir)
@@ -75,15 +93,17 @@ class LamaHDataset(Dataset):
         assert danube_gauges.issubset(connected_gauges)
 
         feasible_gauges = set()
+        qobs_statistics = pd.DataFrame(columns=["mean", "std", "min", "median", "max"], index=pd.Index([], name="ID"))
         for gauge_id in tqdm(danube_gauges, desc="Gauge filtering"):
             ts = pd.read_csv(f"{self.raw_dir}/{self.raw_file_names[1]}/hourly/ID_{gauge_id}.csv",
                              sep=";", usecols=["YYYY", "MM", "DD", "hh", "mm", "qobs"])
             if (ts["qobs"] >= 0).all():
                 if ts.iloc[0]["YYYY"] <= 2000:
                     sub_ts = ts[ts["YYYY"] >= 2000]
-                    if len(sub_ts) == (
-                            18 * 365 + 5) * 24:  # 262_992:  # number of hours in 1988-2017, namely (22 * 365 + 8 * 366) * 24
+                    if len(sub_ts) == (18 * 365 + 5) * 24:  # number of hours in 2000-2017
                         feasible_gauges.add(gauge_id)
+                        qobs_statistics.loc[gauge_id] = [ts["qobs"].mean(), ts["qobs"].std(),
+                                                         ts["qobs"].min(), ts["qobs"].median(), ts["qobs"].max()]
                         assert tuple(sub_ts.iloc[0, :5]) == (2000, 1, 1, 0, 0) \
                                and tuple(sub_ts.iloc[-1, :5]) == (2017, 12, 31, 23, 0)
         print("Determined", len(feasible_gauges), "feasible gauges.")
@@ -105,11 +125,13 @@ class LamaHDataset(Dataset):
 
             stream_dist.reset_index()
 
-        stream_dist["strm_slope"] = stream_dist["elev_diff"] / stream_dist["dist_hdn"]
-        assert feasible_gauges.issubset(danube_gauges)
-
         print("Saving final adjacency list to disk...")
+        stream_dist["strm_slope"] = stream_dist["elev_diff"] / stream_dist["dist_hdn"]
+        stream_dist.sort_values(by="ID", inplace=True)
         stream_dist.to_csv(self.processed_paths[0], index=False)
+
+        print("Saving discharge summary statistics to disk...")
+        qobs_statistics.to_csv(self.processed_paths[1], index=True)
 
     def len(self):
         return sum(self.year_sizes)
@@ -120,9 +142,20 @@ class LamaHDataset(Dataset):
         y = year_tensor[:, offset + self.window_size_hrs + (self.lead_time_hrs - 1)]
         return Data(x=x, y=y.unsqueeze(-1), edge_index=self.edge_index, edge_attr=self.edge_attr)
 
+    def mean(self):
+        return self.mean
+
+    def std(self):
+        return self.std
+
+    def subsample_years(self, years):
+        # TODO switch to itertools.pairwise once Colab on Python 3.10
+        year_boundaries = dict(zip(self.years, pairwise(itertools.accumulate(self.year_sizes, initial=0))))
+        return self.index_select(list(itertools.chain.from_iterable(range(*year_boundaries[y]) for y in years)))
+
     def _decode_index(self, idx):
         for i, size in enumerate(self.year_sizes):
             idx -= size
             if idx < 0:
-                return self.year_tensors[i], self.window_stride_hrs * (idx + size)
+                return self.year_tensors[i], self.stride_length_hrs * (idx + size)
         raise AssertionError("This cannot happen!")
